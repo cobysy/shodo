f08d85b75f6d1152b113333bbbd91a52c80c17bc
cobysy <cobysy@users.noreply.github.com>
2018-09-24 10:13:28 +0200

    add eslint incl conf jquery & TheShodo,Kazari as globals

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "jquery": true
+    },
+    "globals": {
+        "TheShodo": true,
+        "Kazari": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 5
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
